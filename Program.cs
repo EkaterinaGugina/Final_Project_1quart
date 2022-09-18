6056f26ce55e68d2edb3edd345f9ca4b2a35db29
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-09-19 01:16:06 +0500

    update Program

--- Program.cs
+++ Program.cs
@@ -1,7 +1,20 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше или равна 3 символа. 
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write("[");
+        Console.Write($"{array[i]}, ");
+    } 
+    Console.Write("] ");
+}   
+
+
 Console.Clear();
 string[] StringArray = {"1234", "56789", "-2", "o", "end", "computer science", "{}", "LKJHG"};
+PrintArray(StringArray);
+Console.Write(" ---> ");
 int length = StringArray.Length;
 string[] StringArrayCondition = new string[length];
 for (int i = 0; i < length; i++)
