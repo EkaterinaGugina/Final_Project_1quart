5eb26e12b1aea1f1c11f5e19a262ce014e41cfde
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-09-19 00:30:15 +0500

    startig created program

--- Program.cs
+++ Program.cs
@@ -1,83 +1,12 @@
-﻿// /*Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше или равна 3 символа. 
-// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма*/
-
-// Программа, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше или равна 3 символа. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма
 Console.Clear();
-string[] array = { "Rus", "QA", "number", "163", "Hello world!", "qwe" };
-string[] result = new string[array.Length];
-for (int i = 0; i < array.Length; i++)
+string[] StringArray = {"1234", "56789", "-2", "o", "end", "computer science", "{}", "LKJHG"};
+int length = StringArray.Length;
+string[] StringArrayCondition = new string[length];
+for (int i = 0; i < length; i++)
 {
-    if (array[i].Length <= 3) result[i] = array[i];
-    else result[i] = string.Empty;
-    if (result[i] != string.Empty) Console.Write($"{result[i]} ");
+    if (StringArray[i].Length <= 3) StringArrayCondition[i] = StringArray[i];
+    else StringArrayCondition[i] = string.Empty;
+    if (StringArrayCondition[i] != string.Empty) Console.Write($"{StringArrayCondition[i]} ");
 }
-
-
-Console.Clear();
-Console.WriteLine("Введите данные через пробел");
-string[] tempArray = Console.ReadLine().Split(' ').ToArray();
-int count = 0;
-for (int i = 0; i < tempArray.Length; i++)
-{
-    if (tempArray[i].Length <= 3) count++;
-}
-string[] result = new string[count];
-for (int i = 0; i < result.Length; i++)
-{
-    for (int j = 0; j < tempArray.Length; j++)
-    {
-        if (tempArray[j].Length <= 3 && tempArray[j] != string.Empty)
-        {
-            result[i] = tempArray[j];
-            tempArray[j] = string.Empty;
-            break;
-        }
-    }
-    Console.Write($"{result[i]} ");
-}
-
-
-
-
-// Метод автоматического заполнения массива
-string[] AutoFilling(int size, int elementLength)
-{
-    int next = 0;
-    int length = 0;
-    char symbol;
-    string[] array = new string[size];
-    for (int i = 0; i < size; i++)
-    {
-        //Определение длины i-го элемента рандомного массива
-        length = new Random().Next(1, elementLength + 1);
-        for (int j = 0; j < length; j++)
-        {
-            //Определение символа для i-uj элемента рандомного массива
-            next = new Random().Next(32, 128);
-            symbol = (char)next;
-            array[i] += symbol;
-        }
-    }
-    return array;
-}
-// Метод создания результирющего массива
-string[] CreateResultArray(string[] arr)
-{
-    string[] tempArray = new string[arr.Length];
-    int j = 0;
-    for (int i = 0; i < arr.Length; i++)
-    {
-        if (arr[i].Length <= 3)
-        {
-            tempArray[j] = arr[i];
-            j++;
-        }
-    }
-    string[] resultArray = new string[j];
-    for (int i = 0; i < j; i++)
-        resultArray[i] = tempArray[i];
-    return resultArray;
-}
-
-
-    
